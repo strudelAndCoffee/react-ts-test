c7e08f6e0768c2c8b79678d92deac5e260e70d64
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-09-20 16:11:33 -0500

    add sample options for Select props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { Select } from './components/Select';
 
+const options = [
+  { label: "First", value: 1 },
+  { label: "Second", value: 2 },
+  { label: "Third", value: 3 },
+  { label: "Fourth", value: 4 },
+  { label: "Fifth", value: 5 }
+]
+
 function App() {
   return (
     <>
-      <Select />
+      <Select options={options} />
     </>
   )
 }
--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -2,7 +2,7 @@ import styles from '../css/select.module.css';
 
 type SelectOption = {
     label: string
-    value: string
+    value: any
 }
 
 type SelectProps = {
@@ -14,7 +14,15 @@ type SelectProps = {
 export function Select({ value, onChange, options }: SelectProps) {
     return (
         <div className={styles.container}>
-
+            <span className={styles.value}>Value</span>
+            <button className={styles["clear-btn"]}>&times;</button>
+            <div className={styles.divider}></div>
+            <div className={styles.caret}></div>
+            <ul className={styles.options}>
+                {options.map(option => (
+                    <li key={option.label} className={styles.option}>{option.label}</li>
+                ))}
+            </ul>
         </div>
     )
 }
